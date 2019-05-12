d796c796e776af600db38c78bde4611551997c03
Kang-hee Cho <kangchocho@gmail.com>
2019-05-11 23:07:58 -0400

    Initial Commit

---
+++ Weekly Lifts/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Weekly Lifts
+//
+//  Created by Kang-hee cho on 5/11/19.
+//  Copyright © 2019 Kang-hee Cho. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
